f252235e3e0100256c462885e3458ef92329c6ad
unknown <jjieun@inha.edu>
2025-02-11 16:40:07 +0900

    v0.6 while1

--- day02_2.py
+++ day02_2.py
@@ -1,5 +1,5 @@
 # v1.1) for -> while
-# v1.2) while 구문으로 구간 소수를 출력하는 프로그램을 작성
+# v1.2) while 구문으로 구간 소수를 출력하는 프로그램을 작성123
 # v1.3) **  대신 pow 함수
 
 def is_prime(number) -> bool:
@@ -21,12 +21,25 @@ def is_prime(number) -> bool:
     return True
 
 
-# main
-help(is_prime)
+def print_prime_range(start, end):
+    """
+    Prints all prime numbers in the given range [start, end]
+    :param start: starting number
+    :param end: ending number
+    """
+    print(f"Prime numbers between {start} and {end}:")
 
-number = int(input("Input number: "))
+    num = start
+    while num <= end:
+        if is_prime(num):
+            print(num, end=" ")
+        num += 1
 
-if is_prime(number):
-    print(f"{number} is a prime number")
-else:
-    print(f"{number} is NOT a prime number")
+    print()
+
+
+start = int(input("Enter start number: "))
+end = int(input("Enter end number: "))
+
+print_prime_range(start, end)
+
